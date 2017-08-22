2bcbd049e0fa0fe5c077e6ba28ae130ae509da0c
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-08-22 09:55:04 +0200

    added test for exp_date

--- atm/lib/account.rb
+++ atm/lib/account.rb
@@ -1,19 +1,13 @@
 require 'date'
 
 class Account
-  attr_accessor
-  :account_owner
-  :pin_code
-  :exp_date
-  :account_status
-  :balance
+  STANDARD_VALIDITY_YRS = 5
+
+  attr_accessor :account_owner, :pin_code, :exp_date, :account_status, :balance
 
   def initialize
-    @account_owner
     @pin_code = rand(1000..9999)
-    @exp_date = Date.today.next_year(5).strftime("%m/%y")
-    @account_status
-    @balance
+    @exp_date = Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
   end
 
 end
--- atm/spec/account_spec.rb
+++ atm/spec/account_spec.rb
@@ -3,9 +3,14 @@ require 'date'
 
 describe Account do
   it 'checks that the pin is 4 numbers' do
-    number = rand(1000..9999)
-    number_length = Math.log10(number).to_i + 1
-    expect(number_length).to eq 4
+    pin_code = subject.pin_code
+    pin_code_length = Math.log10(pin_code).to_i + 1
+    expect(pin_code_length).to eq 4
   end
-  
+
+  it 'is expected to to have an expiry date on initialize' do
+    expected_date = Date.today.next_year(5).strftime("%m/%y")
+    expect(subject.exp_date).to eq expected_date
+  end
+
 end
