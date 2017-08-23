eb48a68f726ad232d37416f0dc9f5b58a5fe2e04
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-08-23 15:40:07 +0200

    cleaned up error messages

--- atm/lib/atm.rb
+++ atm/lib/atm.rb
@@ -9,22 +9,28 @@ class Atm
 
   def withdraw(amount, pin_code, account)
     case
-    when insufficient_funds_in_account?(amount, account)
-      { status: false, message: 'insufficient funds in account', date: Date.today }
-    when insufficient_funds_in_atm?(amount)
-      { status: false, message: 'insufficient funds in ATM', date: Date.today }
-    when incorrect_pin?(pin_code, account.pin_code)
-      { status: false, message: 'wrong pin', date: Date.today }
-    when card_expired?(account.exp_date)
-      { status: false, message: 'card expired', date: Date.today }
-    when card_not_active?(account.account_status)
-      { status: false, message: 'card disabled', date: Date.today }
+    when insufficient_funds_in_account?(amount, account) then
+      error_message('insufficient funds in account')
+    when insufficient_funds_in_atm?(amount) then
+      error_message('insufficient funds in ATM')
+    when incorrect_pin?(pin_code, account.pin_code) then
+      error_message('wrong pin')
+    when card_expired?(account.exp_date) then
+      error_message('card expired')
+    when card_not_active?(account.account_status) then
+      error_message('card disabled')
     else
       perform_transaction(amount, account)
     end
   end
 
   private
+
+  def error_message(message)
+    { status: false,
+      message: message,
+      date: Date.today }
+  end
 
   def insufficient_funds_in_account?(amount, account)
     amount > account.balance
